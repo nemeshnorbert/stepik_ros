9672b7af6e7e39dd31596306cbe4e239d7b4c8e9
Norbert Nemesh <norberrt@yandex-team.ru>
2018-08-25 21:54:19 +0300

    Add task 2.1.2

---
+++ task_2_1_2/workspace/src/subscriber/src/subscriber.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+
+
+void recieve(const std_msgs::Int32& value) {
+  std::cout << ros::this_node::getName() << ": data: " << value.data;
+}
+
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "subscriber");
+  ros::NodeHandle handle;
+  ros::Subscriber subscriber = handle.subscribe("/topic", 1000, recieve);
+  ros::spin();
+  return 0;
+}
