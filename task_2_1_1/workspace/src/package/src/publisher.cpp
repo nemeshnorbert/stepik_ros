53f10b96f8c19cd9cb43dc01050f857ee034447a
Norbert Nemesh <norberrt@yandex-team.ru>
2018-08-25 20:07:29 +0300

    Add task 2.1

---
+++ task_2_1_1/workspace/src/package/src/publisher.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "publisher");
+  ros::NodeHandle handle;
+  ros::Publisher publisher = handle.advertise<std_msgs::Int32>("/topic", 1000);
+
+  ros::Rate loop_rate(10);
+  std_msgs::Int32 value;
+  while (std::cin) {
+    std::cin >> value.data;
+    publisher.publish(value);
+    loop_rate.sleep();
+  }
+  ros::spinOnce();
+}
